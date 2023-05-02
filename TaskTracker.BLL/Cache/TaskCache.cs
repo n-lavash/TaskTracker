ba287e3851bc037fc00fc58bdc1dffea3e5c5311
n-lavash <levashkinaanastasia@gmail.com>
2023-05-03 02:27:23 +0400

    no message

--- TaskTracker.BLL/Cache/TaskCache.cs
+++ TaskTracker.BLL/Cache/TaskCache.cs
@@ -5,27 +5,28 @@ using System.Linq;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using TaskTrackerWeb.Entity;
 
 namespace TaskTracker.BLL.Cache
 {
     public static class TaskCache
     {
-        public static void SetTask(this ConnectionMultiplexer connectionMultiplexer, TaskTrackerWeb.Entity.UserTask task)
+        public static void SetTask(this ConnectionMultiplexer connectionMultiplexer, UserTask task)
         {
             var taskJson = JsonSerializer.Serialize(task);
-            connectionMultiplexer.GetDatabase().StringSet(task.Id.ToString(), taskJson);
+            connectionMultiplexer.GetDatabase().StringSet(task.Id.ToString() + "task", taskJson);
         }
 
         public static void DeleteTask(this ConnectionMultiplexer connectionMultiplexer, int id)
         {
-            connectionMultiplexer.GetDatabase().KeyDelete(id.ToString());
+            connectionMultiplexer.GetDatabase().KeyDelete(id.ToString()+"task");
         }
 
-        public static List<TaskTrackerWeb.Entity.UserTask> GetTasks(this ConnectionMultiplexer connectionMultiplexer)
+        public static List<UserTask> GetTasks(this ConnectionMultiplexer connectionMultiplexer)
         {
-            RedisKey[] keys = connectionMultiplexer.GetServer("redis:6379").Keys().ToArray();
+            RedisKey[] keys = connectionMultiplexer.GetServer("redis:6379").Keys().Where(key => key.ToString().EndsWith("task")).ToArray();
             var values = connectionMultiplexer.GetDatabase().StringGet(keys);
-            return values.Select(key => JsonSerializer.Deserialize<TaskTrackerWeb.Entity.UserTask>(key!)!).ToList();
+            return values.Select(key => JsonSerializer.Deserialize<UserTask>(key!)!).ToList();
         }
     }
 }
--- TaskTracker.BLL/Cache/UserCache.cs
+++ TaskTracker.BLL/Cache/UserCache.cs
@@ -5,22 +5,23 @@ using System.Linq;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using TaskTrackerWeb.Entity;
 
 namespace TaskTracker.BLL.Cache
 {
     public static class UserCache
     {
-        public static void SetUser(this ConnectionMultiplexer connectionMultiplexer, TaskTrackerWeb.Entity.User user)
+        public static void SetUser(this ConnectionMultiplexer connectionMultiplexer, User user)
         {
             var userJson = JsonSerializer.Serialize(user);
-            connectionMultiplexer.GetDatabase().StringSet(user.Id.ToString(), userJson);
+            connectionMultiplexer.GetDatabase().StringSet(user.Id.ToString() + "user", userJson);
         }
 
-        public static List<TaskTrackerWeb.Entity.User> GetUsers(this ConnectionMultiplexer connectionMultiplexer)
+        public static List<User> GetUsers(this ConnectionMultiplexer connectionMultiplexer)
         {
-            RedisKey[] keys = connectionMultiplexer.GetServer("redis:6379").Keys().ToArray();
+            RedisKey[] keys = connectionMultiplexer.GetServer("redis:6379").Keys().Where(key => key.ToString().EndsWith("user")).ToArray();
             var values = connectionMultiplexer.GetDatabase().StringGet(keys);
-            return values.Select(key => JsonSerializer.Deserialize<TaskTrackerWeb.Entity.User>(key!)!).ToList();
+            return values.Select(key => JsonSerializer.Deserialize<User>(key!)!).ToList();
         }
     }
 }
